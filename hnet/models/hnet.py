2331d83d6e8761d899ce84c91b2a6d7d99f3112e
Edgar Marroquin <emarro@cs.cornell.edu>
2025-10-27 14:08:24 -0400

    Let arch_layout be list of omegaconf list_config

--- hnet/models/hnet.py
+++ hnet/models/hnet.py
@@ -13,6 +13,7 @@ from hnet.modules.dc import (
     DeChunkState,
 )
 from hnet.modules.utils import apply_optimization_params
+from omegaconfg import OmegaConf
 
 from .config_hnet import HNetConfig
 
@@ -26,6 +27,7 @@ class STE(torch.autograd.Function):
     def backward(ctx, grad_output):
         grad_x = grad_output
         return grad_x
+
 
 def ste_func(x):
     return STE.apply(x)
@@ -58,7 +60,9 @@ class HNet(nn.Module):
         for _ in range(stage_idx):
             arch_layout = arch_layout[1]
 
-        assert isinstance(arch_layout, list), f"Wrong arch_layout: {arch_layout}"
+        assert isinstance(arch_layout, list) or OmegaConf.is_list(arch_layout), (
+            f"Wrong arch_layout: {arch_layout} with type {type(arch_layout)}"
+        )
         if len(arch_layout) == 3:
             sub_model_names = ["encoder", "main_network", "decoder"]
             self.is_innermost = False
@@ -116,36 +120,54 @@ class HNet(nn.Module):
             )
         else:
             self.pad_dimension = None
-    
 
-    def _init_weights(self, initializer_range: float = 0.02, parent_residuals: int = 0) -> None:
+    def _init_weights(
+        self, initializer_range: float = 0.02, parent_residuals: int = 0
+    ) -> None:
         n_residuals = parent_residuals
         if self.is_innermost:
             n_residuals += self.main_network.height
             for name, m in self.main_network.named_modules():
-                if isinstance(m, nn.Linear) and not getattr(m.weight, "_no_reinit", False):
+                if isinstance(m, nn.Linear) and not getattr(
+                    m.weight, "_no_reinit", False
+                ):
                     if "out_proj" in name or "fc2" in name:
-                        nn.init.normal_(m.weight, mean=0.0, std=initializer_range / (n_residuals ** 0.5))
+                        nn.init.normal_(
+                            m.weight,
+                            mean=0.0,
+                            std=initializer_range / (n_residuals**0.5),
+                        )
                     else:
                         nn.init.normal_(m.weight, mean=0.0, std=initializer_range)
 
         else:
             n_residuals += self.encoder.height + self.decoder.height
             for name, m in self.encoder.named_modules():
-                if isinstance(m, nn.Linear) and not getattr(m.weight, "_no_reinit", False):
+                if isinstance(m, nn.Linear) and not getattr(
+                    m.weight, "_no_reinit", False
+                ):
                     if "out_proj" in name or "fc2" in name:
-                        nn.init.normal_(m.weight, mean=0.0, std=initializer_range / (n_residuals ** 0.5))
+                        nn.init.normal_(
+                            m.weight,
+                            mean=0.0,
+                            std=initializer_range / (n_residuals**0.5),
+                        )
                     else:
                         nn.init.normal_(m.weight, mean=0.0, std=initializer_range)
             for name, m in self.decoder.named_modules():
-                if isinstance(m, nn.Linear) and not getattr(m.weight, "_no_reinit", False):
+                if isinstance(m, nn.Linear) and not getattr(
+                    m.weight, "_no_reinit", False
+                ):
                     if "out_proj" in name or "fc2" in name:
-                        nn.init.normal_(m.weight, mean=0.0, std=initializer_range / (n_residuals ** 0.5))
+                        nn.init.normal_(
+                            m.weight,
+                            mean=0.0,
+                            std=initializer_range / (n_residuals**0.5),
+                        )
                     else:
                         nn.init.normal_(m.weight, mean=0.0, std=initializer_range)
-                    
+
             self.main_network._init_weights(initializer_range, n_residuals)
-    
 
     def _apply_lr_multiplier(self, lr_multiplier: list[float]) -> None:
         """
@@ -153,8 +175,10 @@ class HNet(nn.Module):
         """
         # a little stupid: we apply lr_multiplier to all parameters, and then for the main stage (which may have another hierarchy), we just apply it again there.
         for param in self.parameters():
-            apply_optimization_params(param, lr_multiplier=lr_multiplier[self.stage_idx])
-        
+            apply_optimization_params(
+                param, lr_multiplier=lr_multiplier[self.stage_idx]
+            )
+
         if not self.is_innermost:
             self.main_network._apply_lr_multiplier(lr_multiplier)
 
@@ -217,9 +241,9 @@ class HNet(nn.Module):
         if inference_params is None:
             inference_params = HNetState(main_network_state=None)
         else:
-            assert (
-                mask is not None
-            ), "Mask must be provided if inference_params is provided"
+            assert mask is not None, (
+                "Mask must be provided if inference_params is provided"
+            )
 
         D = hidden_states.shape[-1]
         EARLY_DIMS = hidden_states.shape[:-1]
@@ -284,7 +308,9 @@ class HNet(nn.Module):
         )
 
         hidden_states = self.residual_func(
-            hidden_states.to(dtype=residual.dtype), residual, bpred_output.selected_probs
+            hidden_states.to(dtype=residual.dtype),
+            residual,
+            bpred_output.selected_probs,
         ).to(hidden_states.dtype)
 
         hidden_states = self.decoder(
@@ -346,7 +372,9 @@ class HNet(nn.Module):
         )
 
         hidden_states = self.residual_func(
-            hidden_states.to(dtype=residual.dtype), residual, bpred_output.selected_probs
+            hidden_states.to(dtype=residual.dtype),
+            residual,
+            bpred_output.selected_probs,
         ).to(hidden_states.dtype)
 
         hidden_states = self.decoder.step(hidden_states, inference_params.decoder_state)
